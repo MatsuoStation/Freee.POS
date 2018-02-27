b03ae9a2ffff5bb3855aa4dd927598b99ae69b7b
Mr24 <sys+github@verysvery.com>
2018-02-27 14:06:10 -0800

    MatsuoStation.Freee.Pos : MySQL.py - Ver.0.0.2 Modify
    Changes to be committed:
    	modified:   MySQL.py - import.json : Setup

--- MySQL.py
+++ MySQL.py
@@ -5,25 +5,30 @@
 #//|                                                 Since:2018.02.27 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|         "VsV.POSforFreee.MySQL.py - Ver.0.0.1 Update:2018.02.27" |
+#//|         "VsV.POSforFreee.MySQL.py - Ver.0.0.2 Update:2018.02.27" |
 #//+------------------------------------------------------------------+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 ### Python3 : Setup ###
+import json
 from urllib.parse import urlparse
 
 ### MySQL : Setup ###
 import mysql.connector
 
 
-### AWS.RDS : Setup ###
-config = {
-	'user': '<YOUR AWS RDS USER NAME>',
-	'password': '<YOUR AWS RDS PASSWORD>',
-	'host': '<YOUR AWS RDS HOST URL>',
-	'database': '<YOUR AWS RDS DATABASE NAME>',
-}
+### JSON : Setup ###
+try:
+	with open ('awsconfig.json', 'r') as f:
+		data = json.load(f)
+		# print(data)
+except json.JSONDecodeError as e:
+	print('JSONDecodeError: ' , e)
+
+
+### MySQL : Connector ###
+config = data
 
 cnn = mysql.connector.connect(**config)
 cur = cnn.cursor(buffered=True)
